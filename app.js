21517465bb2e3357097f9766c070a910d4869335
kasun Chandimal <39762853+kasunChandi@users.noreply.github.com>
2020-11-06 10:45:50 +0530

    initial commit
    create new route for desktop items

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const ecpress = require("express");
+const express = require("express");
 const cors = require("cors"); 
 const mongoose = require("mongoose");
+const DesktopPc = require("./routes/DesktopPc");
+const app = express();
+const port = 5000;
+
+
+app.use(express.json());
+app.use(cors());
+app.use('/api/desktopPC', DesktopPc);
+
+mongoose
+.connect("mongodb://localhost/ComputeShopDB",{useNewUrlParser:true , useUnifiedTopology: true})
+.then(()=>console.log("connetion is success"))
+.catch(err=> console.log("connection error ", err));
+
+
+
+app.listen(port , function(){
+console.log("Listening in port : " +port);
+});
---
+++ routes/DesktopPc.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const DesktopPc = require("../models/DesktopPCSchema");
+
+router.post("/", async (req, res) => {
+    if (!req.body.itemCode) {
+      return res.status(400).send("Not all mandatory values have been set!");
+    }
+  
+    try {
+      let ItemToBeAdd = new DesktopPc({
+        itemCode: req.body.itemCode,
+        itemName: req.body.itemName,
+        stock: req.body.stock,
+        itemDescription: req.body.itemDescription,
+        imgUrl: req.body.imgUrl,
+        itemQty: req.body.itemQty,
+        itemPrice: req.body.itemPrice,
+        warranty:req.body.warranty,
+        likeCount: req.body.likeCount,
+      });
+  
+      ItemToBeAdd = await ItemToBeAdd.save();
+      res.send(ItemToBeAdd);
+    } catch (e) {
+      return res.status(500).send(e.message);
+    }
+  });
+
+  router.get("/", async (req, res) => {
+    try{
+     let itemList = await DesktopPc.find();
+     res.send(itemList);
+    }
+     catch(e){
+       return res.status(404).send(e.message);
+     }
+   });
+
+
+
+
+  module.exports = router;
